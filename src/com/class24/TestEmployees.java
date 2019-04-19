5095f5dbefc245f85f0012b8ca8d42f21c276aed
selimkerim73 <selimkerim73@gmail.com>
2019-04-18 21:55:27 -0400

    class 25

--- src/com/class24/TestEmployees.java
+++ src/com/class24/TestEmployees.java
@@ -3,8 +3,13 @@ package com.class24;
 public class TestEmployees {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Employee emp = new Employee();
+		FullTimeEmployee ft = new FullTimeEmployee();
+		Contractor contractor = new Contractor();
 
+		emp.getPaid();// Employee gets paid salary
+		ft.getPaid();// Full time employee gets paid salary + bonus
+		contractor.getPaid();// Contractor gets paid hourly
 	}
 
 }
